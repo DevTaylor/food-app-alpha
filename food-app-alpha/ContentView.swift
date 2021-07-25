87afb088299b59f2ffa81f037d564d2fe6bab515
Rainier Dirawatun <r.dirawatun@gmail.com>
2021-07-25 14:15:48 -0700

    Initial Commit

---
+++ food-app-alpha/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  food-app-alpha
+//
+//  Created by Rainier Dirawatun on 7/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ food-app-alpha/food_app_alphaApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_app_alphaApp.swift
+//  food-app-alpha
+//
+//  Created by Rainier Dirawatun on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct food_app_alphaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
