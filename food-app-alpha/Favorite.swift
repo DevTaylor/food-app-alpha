a6689341d2453f0090363440cb5916ea9c5b4268
Mark <menm619@gmail.com>
2021-07-25 19:27:10 -0700

    add search bar

--- food-app-alpha/Favorite.swift
+++ food-app-alpha/Favorite.swift
@@ -17,14 +17,15 @@ let sampleFavorite = Favorite(imageName: "ironman")
 struct MockData {
     
     static let favoritesArray = [
-        Favorite(imageName: "ironman"),
-        Favorite(imageName: "ironman"),
-        Favorite(imageName: "ironman"),
-        Favorite(imageName: "ironman"),
-        Favorite(imageName: "ironman"),
-        Favorite(imageName: "ironman"),
-        Favorite(imageName: "ironman"),
-        Favorite(imageName: "ironman")
+        Favorite(imageName: "chickfila"),
+        Favorite(imageName: "sushi"),
+        Favorite(imageName: "bbq"),
+        Favorite(imageName: "pho"),
+        Favorite(imageName: "chickensoup"),
+        Favorite(imageName: "hamburger"),
+        Favorite(imageName: "curry"),
+        Favorite(imageName: "poke"),
+        Favorite(imageName: "ramen")
         
         ]
     
--- food-app-alpha/ProfileView.swift
+++ food-app-alpha/ProfileView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 let favorite = Favorite(imageName: "")
 
+
 var columns: [GridItem] =
          Array(repeating: .init(.flexible()), count: 3)
 
 struct ProfileView: View {
+    
+    @State private var searchField = ""
+    
     var body: some View {
         NavigationView {
             ScrollView {
@@ -21,9 +25,19 @@ struct ProfileView: View {
                         ForEach(MockData.favoritesArray) {favorite in
                             FoodItemCellView(imageName: favorite.imageName)
                         }
-                                                
                     }
+                    
+                    
+                    CustomTextEditor(placeholder: "What are you craving?", searchText: $searchField)
+                        .padding(.vertical)
+                        .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.secondary, lineWidth: 1.0).frame(height: 45))
+                        .padding(.horizontal)
+                        
+                        
+
+                        
                 }
+                .navigationTitle("🍔Foodie")
             }
         }
     }
@@ -42,7 +56,29 @@ struct FoodItemCellView: View {
         VStack {
             Image(imageName)
                 .resizable()
-                .frame(width: 90, height: 90)
+                .frame(width: 120, height: 110)
+        }
+    }
+}
+
+struct CustomTextEditor: View {
+    let placeholder: String
+    @Binding var searchText: String
+    let internalPadding: CGFloat = 5
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if searchText.isEmpty  {
+                Text(placeholder)
+                    .foregroundColor(Color.primary.opacity(0.25))
+                    .padding(EdgeInsets(top: 7, leading: 4, bottom: 0, trailing: 0))
+                    .padding(internalPadding)
+            }
+            TextEditor(text: $searchText)
+                .padding(internalPadding)
+        }.onAppear() {
+            UITextView.appearance().backgroundColor = .clear
+        }.onDisappear() {
+            UITextView.appearance().backgroundColor = nil
         }
     }
 }
