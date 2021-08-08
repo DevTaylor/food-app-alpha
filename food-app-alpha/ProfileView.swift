a0c53d7a76ac3f646384e85395e4f2a663d759b5
DevTaylor <32975521+DevTaylor@users.noreply.github.com>
2021-08-08 12:53:02 -0700

    more updates

--- food-app-alpha/ProfileView.swift
+++ food-app-alpha/ProfileView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View
     {
-        VStack(alignment: .center, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content:
+        VStack(alignment: .leading, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/, content:
         {
             // Profile Header
-            HStack(alignment: .center, spacing: 3.0, content:
+            HStack(alignment: .center, spacing: 26.0, content:
             {
                 // Profile picture
                 VStack
@@ -21,33 +21,44 @@ struct ProfileView: View {
                     Image("ProfilePhoto1")
                         .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
                         .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                    Text("R A I N I E R")
+
                     
                 }
                 
                 // Profile info
-                HStack
-                {
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: /*@START_MENU_TOKEN@*/15.0/*@END_MENU_TOKEN@*/, content: {
                     VStack
                     {
                         Text("22")
+                            .font(.system(size: 15))
                             .fontWeight(.bold)
                         Text("Posts")
+                            .font(.system(size:12))
                     }
                     VStack
                     {
                         Text("316")
+                            .font(.system(size: 15))
                             .fontWeight(.bold)
                         Text("Followers")
+                            .font(.system(size:12))
                     }
                     VStack
                     {
                         Text("374")
+                            .font(.system(size: 15))
                             .fontWeight(.bold)
                         Text("Following")
+                            .font(.system(size:12))
                     }
-                }
+                })
+                
             })
+            Text("R A I N I E R")
+                .fontWeight(.medium)
+                .alignmentGuide(.leading) { dimension in
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/dimension[.top]/*@END_MENU_TOKEN@*/
+                }
             
             // Bio Here
             Text("I tried to tell everyone when I was a kid that I had powers - no one wanted to believe me...")
